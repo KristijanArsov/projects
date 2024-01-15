934d9b2928e38258b8282073e4d634b575475349
Your Name <you@example.com>
2024-01-15 17:14:03 +0100

    added to branch

---
+++ starter/src/components/RelatedProducts.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ProductItem from "./ProductItem";
+import { ProductType } from "../type/type";
+
+interface Props {
+  data: ProductType[]
+}
+
+const RelatedProducts: React.FC<Props> = ({data}) => {
+  return (
+    <section className="sec-relate-product bg0 p-t-45 p-b-105">
+      <div className="container">
+        <div className="p-b-45">
+          <h3 className="ltext-106 cl5 txt-center">Related Products</h3>
+        </div>
+
+        <div className="wrap-slick2">
+          <div className="d-flex">
+           {
+            data.map((product) => {
+              return  <ProductItem key={product.id} {...product} />
+            })
+           }
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default RelatedProducts;
---
+++ starter/src/type/type.ts
@@ -0,0 +1,42 @@
+export interface ProductsType {
+  id: string;
+  price: string;
+  title: string;
+  gender: string;
+  img: string;
+  description: string;
+}
+
+export interface FeaturedBlogsType {
+  id: string;
+  author: string;
+  date: string;
+  category: string;
+  excerpt: string;
+  img: string;
+  title: string;
+  first_content: string;
+  second_content: string;
+}
+
+export interface AboutType {
+  title: string;
+  first_content: string;
+  second_content: string;
+  third_content: string;
+  first_image: string;
+  second_image: string;
+  second_title: string;
+  fourth_content: string;
+  fifth_content: string;
+  author: string;
+}
+
+export interface ProductType {
+  id: string;
+  price: string;
+  title: string;
+  gender: string;
+  img: string;
+  description: string;
+}
